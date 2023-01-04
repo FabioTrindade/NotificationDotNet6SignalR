8d1d11d60f13b74bac3c00da2b9d8279c1245404
FabioTrindade <fabio.trindade@hotmail.com>
2023-01-04 11:41:55 +0000

    dump version

---
+++ NotificationDotNet6SignalR/wwwroot/js/notification.js
@@ -0,0 +1,20 @@
+﻿$(function(){
+    $('#IsRead').on('change.bootstrapSwitch', function(e) {
+        const id = e.target.dataset.notificationId;
+        const status = e.target.checked;
+
+        ChangeStatus(id, status);
+    });
+
+    function ChangeStatus(id, status) {
+        sendRequest('PUT', '/Notification/ChangeRead', 'json', { id, status } , (retorno) => {
+            if (!retorno.data.success) {
+                showAlert('warning', 'Mensagem Retorno : ', retorno.data.message, () => { }, () => { });
+                return;
+            }
+            toast('success', 'Notificação alterada com sucesso!');
+        }, (err) => {
+            showAlert('info', 'Ooops!', 'Verifique sua conexão e tente novamente', () => { });
+        })
+    }
+});
